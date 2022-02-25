f74d47965d93517ca268700533988f7ede10db50
Jon Frisby <jfrisby@mrjoy.com>
2022-02-25 14:28:12 -0800

    Attempt to get deployment IAM constructions sorted out.

--- iam.tf
+++ iam.tf
@@ -1,18 +1,12 @@
-# data "aws_iam_policy_document" "assume-role" {
-#   statement {
-#     actions = ["sts:AssumeRole"]
-#     principals {
-#       type        = "Service"
-#       identifiers = ["ec2.amazonaws.com"]
-#     }
-#   }
-# }
-
-# resource "aws_iam_role" "static-site-deployer" {
-#   name = join("-", [var.name, "deployer"])
-
-#   assume_role_policy = data.aws_iam_policy_document.assume-role.json
-# }
+data "aws_iam_policy_document" "assume-role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
 
 resource "aws_iam_policy" "static-site-deployer" {
   name        = join("-", [var.name, "deployer"])
@@ -44,4 +38,15 @@ resource "aws_iam_policy" "static-site-deployer" {
       }
     ]
   })
+}
+
+resource "aws_iam_role" "static-site-deployer" {
+  name = join("-", [var.name, "deployer"])
+
+  assume_role_policy = data.aws_iam_policy_document.assume-role.json
+}
+
+resource "aws_iam_role_policy_attachment" "static-site-deployer" {
+  role       = aws_iam_role.static-site-deployer.id
+  policy_arn = aws_iam_policy.static-site-deployer.arn
 }
